d553810e46c625105f2057e55c049427a6ed1424
MUAZMMIL ALI <alimuzmmil294@gmail.com>
2026-08-27 20:46:45 +0500

    Learning Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,10 @@ const App = () => {
   return (
     <div>
       <img src={image} alt="Image...." />
-      <h1>Hello world from Muzammil Ali</h1>
+      <h1>Hello world from Muzammil Ali</h1> 
+      <h1>Hello world from Muzammil Ali</h1> 
+      <h1>Hello world from Muzammil Ali</h1> 
+      <h1>Hello world from Muzammil Ali</h1> 
     </div>
   );
 };
